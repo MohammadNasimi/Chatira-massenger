2a9b4dd7432d092b4b368b22471d9573fed9c4eb
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-07 23:59:46 +0330

    add restframework setttings

--- Chatiraconfig/settings.py
+++ Chatiraconfig/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'accounts',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +144,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 ############
 AUTH_USER_MODEL = 'accounts.User'
+#### rest framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
