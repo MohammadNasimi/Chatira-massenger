bfaa2aeeda2c94627c5138638af1f45af221c066
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-07 15:26:41 +0330

    create user model in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser,UserManager
+
+
+class MyUserManager(UserManager):
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['phone']
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=11, null=True, blank=True, unique=True)
+    username = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    bio = models.CharField(max_length=20, null=True, blank=True, unique=True)
+
+    USERNAME_FIELD = ['phone']
+    objects = MyUserManager()
