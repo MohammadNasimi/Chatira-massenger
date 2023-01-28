9418446265a3e635dd6b724488731aecfe0aadb3
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-29 00:45:34 +0330

    UserMetaSerializer

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from accounts.models import User,profile
+from accounts.models import User,profile,user_meta
 class LoginSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -13,4 +13,10 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = profile
         fields = ('id', 'user', 'username','bio', 'profile_pic')
         read_only_fields = ('id','user')
+
+class UserMetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_meta
+        fields = ('id', 'profile', 'personal_theme','status', 'last_seen')
+        read_only_fields = ('id','profile')
         
