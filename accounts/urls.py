1e6ebf79615c2b796c8f98d1aedb2cec002a0938
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-29 00:22:01 +0330

    ProfileView urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
 from rest_framework_simplejwt import views as jwt_views
-from accounts.views import  LoginView,RegisterView
+from accounts.views import  LoginView,RegisterView,ProfileView
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/detail/<int:pk>/', ProfileView.as_view(), name='profile_detail'),
+
 
 ]
