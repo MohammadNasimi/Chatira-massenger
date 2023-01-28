0c655371a3c7c97c67f1a8e8dce072f1270e6b9b
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-29 00:21:47 +0330

    ProfileSerializer add

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from accounts.models import User
+from accounts.models import User,profile
 class LoginSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -7,4 +7,10 @@ class LoginSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'phone', 'password','email', 'first_name', 'last_name')
         read_only_fields = ('id','email', 'first_name', 'last_name')
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = profile
+        fields = ('id', 'user', 'username','bio', 'profile_pic')
+        read_only_fields = ('id','user')
         
