4b1f08bd437f592ea7cd0dff932efaafbda5cdaa
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-29 00:45:50 +0330

    UserMetaView urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path 
 from rest_framework_simplejwt import views as jwt_views
-from accounts.views import  LoginView,RegisterView,ProfileView
+from accounts.views import  LoginView,RegisterView,ProfileView,UserMetaView
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/detail/<int:pk>/', ProfileView.as_view(), name='profile_detail'),
+    path('user_meta/detail/<int:pk>/', UserMetaView.as_view(), name='user_meta_detail'),
 
 
 ]
