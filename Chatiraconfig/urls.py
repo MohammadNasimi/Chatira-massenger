8ef0f9b980cabcc9d9fa721fe00cfb861950d3e1
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 15:53:29 +0330

    add contacts urls in chatiraconfig

--- Chatiraconfig/urls.py
+++ Chatiraconfig/urls.py
@@ -20,4 +20,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
+    path('contact/',include('contact.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
