d680ff6b12b72c672061251e3ea2c2f5339881c5
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 17:42:54 +0330

    add django filter in settings

--- Chatiraconfig/settings.py
+++ Chatiraconfig/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'core',
     'accounts',
     'contact',
+    'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -152,4 +153,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+
 }
