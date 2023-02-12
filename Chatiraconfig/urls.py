e7f73fef6adebd7f0e29bf4a8db05179813345b7
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 20:41:52 +0330

    add swagger urls

--- Chatiraconfig/urls.py
+++ Chatiraconfig/urls.py
@@ -17,9 +17,46 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf.urls.static import static
 from django.conf import settings
+##########swagger url setting
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi 
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+class CustomOpenAPISchemaGenerator(OpenAPISchemaGenerator):
+  def get_schema(self, request=None, public=False):
+    """Generate a :class:.Swagger object with custom tags"""
+
+    swagger = super().get_schema(request, public)
+    swagger.tags = [
+        {
+            "name": "Chatira",
+            "description": "Chatira API"
+        },
+    ]
+
+    return swagger
+
+schema_view = get_schema_view( 
+openapi.Info(
+title="Chatira API",
+default_version="v1",
+description="A sample API for learning DRF",
+terms_of_service="https://www.google.com/policies/terms/",
+contact=openapi.Contact(email="hello@example.com"),
+license=openapi.License(name="BSD License"),
+),
+public=True,
+permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('contact/',include('contact.urls')),
+    path('swagger/', schema_view.with_ui( 
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui( 
+        'redoc', cache_timeout=0), name='schema-redoc'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
