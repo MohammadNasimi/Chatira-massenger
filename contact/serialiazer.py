94824454ccd6e5c30d0b4652b9dfe92e617e4848
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 19:22:03 +0330

    add nested serializer user and contact

--- contact/serialiazer.py
+++ contact/serialiazer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from contact.models import contact
+from accounts.serializer import ProfileSerializer
 class ContactSerializer(serializers.ModelSerializer):
+    user =  ProfileSerializer( read_only=True)
+    contact =  ProfileSerializer(read_only=True)
 
     class Meta:
         model = contact
-        fields = ('user','name','contact','block')
+        fields = ('user','name_contract','contact','block')
         only_read_field = ('user')
