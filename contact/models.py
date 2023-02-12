92ee0e7efa3ef13da84d7995aa05afcb5f9bcabe
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 17:01:10 +0330

    change one to one user to foreignkey

--- contact/models.py
+++ contact/models.py
@@ -3,7 +3,7 @@ from core.models import BaseModel
 # Create your models here.
 from accounts.models import profile
 class contact(BaseModel):
-    user = models.OneToOneField(profile,on_delete=models.CASCADE,related_name='user_set')
+    user = models.ForeignKey(profile,on_delete=models.CASCADE,related_name='user_set')
     contact = models.ForeignKey(profile,on_delete=models.CASCADE,related_name='contact_set')
     block = models.BooleanField(default=False)
     
