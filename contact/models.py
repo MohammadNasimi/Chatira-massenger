64a5c93f51858b0f7bdae108203409b9ed9ec8c7
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 15:42:38 +0330

    model contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from core.models import BaseModel
 # Create your models here.
+from accounts.models import profile
+class contact(BaseModel):
+    user = models.OneToOneField(profile,on_delete=models.CASCADE)
+    contact = models.ForeignKey(profile,on_delete=models.CASCADE)
+    block = models.BooleanField(default=False)
+    
