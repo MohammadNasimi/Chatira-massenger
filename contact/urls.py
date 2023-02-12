2bd983ab4509b5f248cedee1f053e4c9b86cb1d1
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 19:23:13 +0330

    create filter url

---
+++ contact/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+from contact.views import  *
+urlpatterns = [
+    path('create/', CreateContactView.as_view(), name='create_contact'),
+    path('filter/', FilterContactView.as_view(), name='filter_contact'),
+]
