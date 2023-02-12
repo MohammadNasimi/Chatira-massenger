6f789cd48058d1dee59ce5606831148e4652fbb1
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-12 19:54:28 +0330

    urls update contact

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from contact.views import  *
 urlpatterns = [
     path('create/', CreateContactView.as_view(), name='create_contact'),
     path('filter/', FilterContactView.as_view(), name='filter_contact'),
+    path('update/<int:pk>/',UpdateContactView.as_view(), name='update_contact'),
 ]
