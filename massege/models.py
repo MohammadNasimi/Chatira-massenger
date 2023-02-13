46605b229c207506fbbd87619d53d71772e366ed
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-14 00:03:00 +0330

    conversations model add

--- massege/models.py
+++ massege/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from core.models import BaseModel
 from accounts.models import profile
+from contact.models import contact
 # Create your models here.
+class conversation(BaseModel):
+    sender = models.ForeignKey(profile,on_delete=models.CASCADE,related_name='sender_set')
+    contact=models.ForeignKey(contact,on_delete=models.CASCADE,related_name='contact_set')
+    def __str__(self) -> str:
+        return {self.sender.user.phone,self.contact}
+    
+    
 content_massege_type= (
            (1, "text"),
            (2, "image"),
            (3, "video"),
 )
+
 class massege(BaseModel):
-    sender = models.ForeignKey(profile,on_delete=models.CASCADE,related_name='sender_set')
-    reciever=models.ForeignKey(profile,on_delete=models.CASCADE,related_name='reciever_set')
+    conversation = models.ForeignKey(conversation,on_delete=models.CASCADE,related_name='conversation_set')
     content=models.TextField()
     date=models.DateTimeField(auto_now_add=True)
     seen_time_stamp = models.DateTimeField(auto_now_add=True)
