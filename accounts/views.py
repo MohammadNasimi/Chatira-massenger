f6992df54f46a1d687ae37f1416a068e6916b531
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-21 13:10:57 +0330

    change login register view for otp

--- accounts/views.py
+++ accounts/views.py
@@ -15,20 +15,29 @@ from django.contrib.auth import authenticate
 # drf-ysg for swagger import
 from drf_yasg.utils import swagger_auto_schema
 from accounts import docs
+#OTP
+from accounts.helper import *
 # log in
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
     @swagger_auto_schema(operation_description=docs.Log_in_Post,tags=['accounts'])
     def post(self, request, *args, **kwargs):
-        if "phone" not in request.data:
-            return Response({"detail": "please send phone"} , status=status.HTTP_400_BAD_REQUEST)
+        if "phone" not in request.data or "otp" not in request.data:
+            return Response({"detail": "please send phone and otp"} , status=status.HTTP_400_BAD_REQUEST)
         def get_token(user):
                 refresh = RefreshToken.for_user(user)
                 return {
                     'refresh':str(refresh),
                     'access':str(refresh.access_token)
                 }
+        user_get = User.objects.get(phone = request.data['phone'])
+        #check otp expire time
+        if not check_otp_expiration(request.data['phone']):
+            return Response({'user':'otp expire time finish '}, status=status.HTTP_200_OK)
 
+        if user_get.otp != int(request.data['otp']):
+            return Response({'user':'wrong otp '}, status=status.HTTP_200_OK)
+        
         user = authenticate(phone = request.data['phone'])
         if user :
             data = LoginSerializer(user).data
@@ -41,7 +50,7 @@ class LoginView(generics.GenericAPIView):
 
             return Response(data, status=status.HTTP_200_OK)
         else:
-            return Response({'user':'wrong username '}, status=status.HTTP_200_OK)
+            return Response({'user':'wrong phone '}, status=status.HTTP_200_OK)
 
 # register
 class RegisterView(APIView):
@@ -57,12 +66,25 @@ class RegisterView(APIView):
             return user
         if request.data.get('phone') == None  :
             return Response({"detail": "please fill phone "} , status=status.HTTP_400_BAD_REQUEST)
+        #send_otp
+        otp = get_random_otp()
+        print(otp)
+        # send_otp(request.data.get('phone'),otp)
+        
         try:
             user_create = create_users()
+            #save otp
+            user_create.otp = otp
+            user_create.save()
             profile_create = profile.objects.create(user =user_create)   
             user_meta.objects.create(profile = profile_create)
         except:
-            return Response({"detail"  : "phone exist"} , status=status.HTTP_400_BAD_REQUEST)
+            user =User.objects.get(phone = request.data.get('phone'))
+            #save otp
+            user.otp = otp
+            user.otp_create_time = datetime.now(timezone.utc)
+            user.save()
+            # return Response({"detail"  : "phone exist"} , status=status.HTTP_400_BAD_REQUEST)
         
         return Response(request.data, status=status.HTTP_201_CREATED)
     
