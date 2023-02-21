4ff96cdf4d8c1b5e41eb145f1a57dbdfa5e051dc
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-02-21 13:51:18 +0330

    connect signals

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
-
+from django.core.signals import request_finished
+from django.db.models.signals import post_save
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    def ready(self):
+        from accounts.signals import my_callback,create_new_user
+        from accounts.models import User
+        request_finished.connect(my_callback)
+        post_save.connect(create_new_user,sender=User)
